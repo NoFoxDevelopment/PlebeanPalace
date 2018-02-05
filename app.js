8c9b8d910c8fd4342e34ae2f3e1ab599ccd2bb7e
NoFoxDevelopment <31908496+NoFoxDevelopment@users.noreply.github.com>
2018-02-04 19:36:59 -0500

    Initial Commit.
    Boilerplate completed with rudimentary auth system in place.
    
    General setup and file structure started.
    
    NPM Init completed

---
+++ app.js
@@ -0,0 +1,64 @@
+const express               = require('express'),
+			mongoose              = require('mongoose'),
+			passport              = require('passport'),
+			LocalStrategy         = require('passport-local'),
+			passportLocalMongoose = require('passport-local-mongoose'),
+			bodyParser            = require('body-parser'),
+			User                  = require ('./models/user.js');
+
+mongoose.connect('mongodb://localhost/plebianpalace');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(require('express-session')({
+	secret: "A still more glorious dawn awaits",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//======
+//ROUTES
+//======
+
+app.get('/', function(req, res) {
+	res.render('login');
+});
+
+app.get('/game', function(req, res) {
+	res.render('game');
+});
+
+
+//Auth Routes
+//show signup form
+app.get('/register', function(req, res) {
+	res.render('register');
+});
+
+//user signup handling
+app.post('/register', function(req, res) {
+	req.body.username
+	req.body.password
+	User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+		err ? (
+			console.log(err),
+			res.render('register')
+		) : (
+			passport.authenticate('local')(req, res, function(){
+				res.redirect('/game');
+			})
+		);
+	});
+});
+
+app.listen(5000, function() {
+	console.log('app being served on port 5000...');
+});
